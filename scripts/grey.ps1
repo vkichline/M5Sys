1cab33c0001e40a72d5bd7f24c58c45851325bfb
Van Kichline <vkichline@hotmail.com>
2020-07-16 21:03:52 -0700

    scripts for Windows

---
+++ scripts/grey.ps1
@@ -0,0 +1,19 @@
+Write-Host "Building for M5Stack Grey"
+$programs       = @('AstroClock', 'ClickBang', 'Clock', 'Distance', 'FileManage', 'HomeControl',
+                    'IChing', 'ImuMonitor', 'Life', 'mDNSMonitor', 'menu', 'MQTTMonitor',
+                    'PacketMonitor', 'SoundMonitor', 'SoundRecorder', 'Weather', 'WiFiScanner')
+$firmware_path  = '.pio/build/m5stack-grey'             # path from project directory to built firmware
+$script_dir     = $PSScriptRoot                         # Location of the script.
+$project_dir    = "$script_dir/.."                      # Location of the project.
+$prop_dir       = "$project_dir/SD"                     # Destination location
+$apps_dir       = "$project_dir/Apps"                   # Location of M5Sys application projects
+$iching_data    = "$apps_dir/IChing/data/hexagrams.*"   # Data files for IChing
+
+Remove-Item "$prop_dir/*"                               # clean the output directory
+New-Item -Path "$prop_dir/data" -ItemType Directory     # create PROP/data
+Copy-Item $iching_data "$prop_dir/data"                 # copy IChing data files to PROP/data
+foreach ($prog in $programs) {
+    Set-Location -Path "$apps_dir/$prog"
+    Invoke-Expression 'platformio run --environment m5stack-grey'
+    Copy-Item "$apps_dir/$prog/$firmware_path/$prog.bin" "$prop_dir/$prog.bin"
+}
---
+++ scripts/prop.ps1
@@ -0,0 +1,24 @@
+# Copies the content from the staging directory for M5Sys to the SD card in drive G:
+
+Write-Host "Copying drop to SD Card in drive G:"
+$files          = @('AstroClock.bin', 'ClickBang.bin', 'Clock.bin', 'Distance.bin',
+                    'FileManager.bin', 'HomeControl.bin', 'IChing.bin', 'ImuMonitor.bin',
+                    'Life.bin', 'mDNSMonitor.bin', 'menu.bin', 'MQTTMonitor.bin',
+                    'PacketMonitor.bin', 'SoundMonitor.bin', 'SoundRecorder.bin',
+                    'Weather.bin', 'WiFiScanner.bin')
+$data_files     = @('data/hexagrams.idx', 'data/hexagrams.isam')
+$script_dir     = $PSScriptRoot                         # Location of the script.
+$project_dir    = "$script_dir/.."                      # Location of the project.
+$prop_dir       = "$project_dir/SD"                     # Drop location
+$dest_dir       = 'G:/'                                 # SD card on my particular system
+$dest_data      = "G:/data"
+                    
+Remove-Item 'G:\*.*' -Exclude 'System Volume Information' -Recurse
+Set-Location -Path "$prop_dir"                          # Copy from SD
+foreach ($file in $files) {
+    Copy-Item $file $dest_dir                           # Copy over all programs
+}
+foreach ($file in $data_files) {
+    Copy-Item $file $dest_data                          # Copy over all data files
+}
+Set-Location -Path "$script_dir"                        # Go back home
